27f3bc4bfa56b0253435376f3b95454346246715
Gabriela Radu <gabrielaradu.developer@gmail.com>
2017-02-15 18:17:27 +0200

    Change background and add layouts for each screen of intro

--- app/src/main/java/com/readingcards/ReadingCardsSplashScreen.java
+++ app/src/main/java/com/readingcards/ReadingCardsSplashScreen.java
@@ -1,6 +1,5 @@
 package com.readingcards;
 
-import com.stephentuso.welcome.BasicPage;
 import com.stephentuso.welcome.ParallaxPage;
 import com.stephentuso.welcome.WelcomeActivity;
 import com.stephentuso.welcome.WelcomeConfiguration;
@@ -14,20 +13,22 @@ public class ReadingCardsSplashScreen extends WelcomeActivity {
     protected WelcomeConfiguration configuration() {
         return new WelcomeConfiguration.Builder(this)
                 .defaultBackgroundColor(R.color.colorAccent)
-                .page(new ParallaxPage(R.layout.splash_screen_parallax,
+                .page(new ParallaxPage(R.layout.splash_screen_one_parallax,
                         "Make your own notes",
                         "Are you preparing to pitch your startup to somebody? Add your notes and keep them safe on your phone.")
                         .lastParallaxFactor(2f)
                         .background(R.color.step_one)
                 )
-                .page(new BasicPage(R.drawable.ic_step1,
+                .page(new ParallaxPage(R.layout.splash_screen_two_parallax,
                         "And...?",
                         "Make collections from your notes and practice reading them with a timer you set.")
+                        .lastParallaxFactor(2f)
                         .background(R.color.step_two)
                 )
-                .page(new BasicPage(R.drawable.ic_step3,
+                .page(new ParallaxPage(R.layout.splash_screen_three_parallax,
                         "Want more features?",
                         "Email the developer: gabrielaradu.developer@gmail.com")
+                        .lastParallaxFactor(2f)
                         .background(R.color.step_three)
                 )
                 .swipeToDismiss(true)
