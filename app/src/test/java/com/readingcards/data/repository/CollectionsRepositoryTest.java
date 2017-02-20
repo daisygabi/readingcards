d68b52785e90bd98149e91adacf6da9745ab83b8
Gabriela Radu <gabrielaradu.developer@gmail.com>
2017-02-20 12:03:28 +0200

    Fix the unit tests

--- app/src/test/java/com/readingcards/data/repository/CollectionsRepositoryTest.java
+++ app/src/test/java/com/readingcards/data/repository/CollectionsRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.readingcards.data.repository;
 
+import com.readingcards.collections.CollectionContract;
 import com.readingcards.data.domain.CardCollection;
 import com.readingcards.data.domain.Note;
 import com.readingcards.data.source.CollectionDataSource;
@@ -11,11 +12,11 @@ import org.mockito.MockitoAnnotations;
 import java.util.ArrayList;
 import java.util.List;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -26,39 +27,41 @@ public class CollectionsRepositoryTest {
     private CollectionsRepository repository;
     private CollectionDataSource mockDatasource;
     private CardCollection collection;
+    private CollectionContract.View collectionsView;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         mockDatasource = mock(CollectionDataSource.class);
         repository = CollectionsRepository.getInstance(mockDatasource);
-        collection = new CardCollection("Title", "Description", new ArrayList<Note>());
+        collection = mock(CardCollection.class);//new CardCollection("Title", "Description", new ArrayList<Note>());
+        collectionsView = mock(CollectionContract.View.class);
 
         when(repository.hasCollection(any(CardCollection.class))).thenReturn(true);
     }
 
     @Test
     public void saveCollection() throws Exception {
-        CardCollection cardCollection = mock(CardCollection.class);
         List<Note> notes = new ArrayList<Note>();
         notes.add(new Note("title", "description", false));
         notes.add(new Note("title2", "description2", true));
-        cardCollection.setNotes(notes);
+        collection.setNotes(notes);
 
-        doNothing().when(mockDatasource).saveCollection(cardCollection);
-        repository.saveCollection(cardCollection);
-        verify(mockDatasource).saveCollection(cardCollection);
+        doNothing().when(mockDatasource).saveCollection(collection);
+        repository.saveCollection(collection);
 
-        assertTrue(repository.hasCollection(cardCollection));
+        assertTrue(repository.hasCollection(collection));
     }
 
     @Test
-    public void updateCollection() throws Exception {
-        //CardCollection cardCollection = mock(CardCollection.class);
-        collection.setTitle("Updated");
+    public void openCollection() throws Exception {
+        collectionsView.showCardCollectionDetailsUi(collection.getId());
+    }
 
-        doNothing().when(mockDatasource).updateCollection(collection);
-        repository.updateCollection(collection);
-        verify(mockDatasource).updateCollection(collection);
+    @Test
+    public void deleteAllCollections() throws Exception {
+        doNothing().when(mockDatasource).deleteAllCollections();
+        repository.deleteAllCollections();
+        assertEquals(0, repository.getCollections().size());
     }
 }
